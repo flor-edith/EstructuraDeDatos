be7bd5ff795e436d90075c4cde6eef34c6cdcd95
flor_edith <floredith907@gmail.com>
2024-09-25 10:15:46 -0600

    segundo commit

---
+++ semana 1/code.js
@@ -0,0 +1,3 @@
+function saluda(){
+    console.log("Hola que hay")
+}
---
+++ semana 5/Variables.js
@@ -0,0 +1,15 @@
+//operadores aritmeticos
+var num1, num2;
+//suma
+num1 = 50;
+console.log(50 + 5);
+console.log(num1 + 5); 
+//resta
+num2 = 20;
+console.log(num1-num2);
+//division
+console.log(250/num1);
+//modulo
+console.log(num1%num2);1
+//prueba de prosedencia
+console.log(3 + 5 * 2 - 8 * 5 / 2) 
